f80200f56e44709017e1612dc7562eb58164d6e2
Jorge Argueta <34260059+Arg45@users.noreply.github.com>
2019-07-24 13:47:38 -0600

    Tarea 2
    en espera de la explicacion de que hacer

--- [EDD]2S19/src/Tarea2/Tarea2.java
+++ [EDD]2S19/src/Tarea2/Tarea2.java
@@ -1,5 +1,22 @@
-package Tarea1;
+package Tarea2;
 
 public class Tarea2 {
     
+    
+    public static void main(String[] args) {
+        Tarea2 t2=new Tarea2();
+        
+    }
 }
+
+//Tipo Bolsa (Elemento)
+//Sintaxis
+//- bolsavacia = Bolsa
+//- insertar(Bolsa,Elemento) = Bolsa
+//- esvacia(Bolsa) = booleano
+//- cuantos(Bolsa,Elemento) = natural
+//Semántica 
+//- esvacia(bolsavacia) = cierto
+//- esvacia(poner(b,e)) = falso
+//- cuantos(bolsavacia,e) = cero
+//- cuantos(poner(b,f),e) = si f=e  sucesor(cuantos(b,e)) | cuantos(b,e)

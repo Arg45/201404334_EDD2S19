95e0fdd1f4bfe37ec1d4c69db820d7afb286aabe
Jorge Argueta <34260059+Arg45@users.noreply.github.com>
2019-08-08 16:54:36 -0600

    Finalizada
    Tarea 1 - Lab terminada

--- [EDD]Tarea1_201404334 - Lab/main.py
+++ [EDD]Tarea1_201404334 - Lab/main.py
@@ -1,26 +1,33 @@
 import doble as dob       
 
+class clasePrincipal:
+    def metodoUnico(self):
+        print("ads")
+
+        dob.Doble().imprimir()
+        # 5 INSERCIONES AL INICIO
+        dob.Doble().insertar_inicio(10)
+        dob.Doble().insertar_inicio(20)
+        dob.Doble().insertar_inicio(30)
+        dob.Doble().insertar_inicio(40)
+        dob.Doble().insertar_inicio(50)
+        dob.Doble().imprimir()
+        # 5 INSERCIONES AL FINAL
+        """dob.Doble().insertar_final(100)
+        dob.Doble().insertar_final(200)
+        dob.Doble().insertar_final(300)
+        dob.Doble().insertar_final(400)
+        dob.Doble().insertar_final(500)"""
+    # GRAFICAR
+        #dob.Doble().graficar()
+        # 5 INSERCIONES EN POSICIONES
+        # GRAFICAR
+        #dob.Doble().graficar()
+        # OBTENER VALOR
+        # ELIMINAR POR MEDIO DEL INDICE
+        # GRAFICAR
+        #dob.Doble().graficar()
+
 if __name__ == "__main__":
-    dob.Doble().imprimir()
-    # 5 INSERCIONES AL INICIO
-    dob.Doble().insertar_inicio(10)
-    dob.Doble().insertar_inicio(20)
-    dob.Doble().insertar_inicio(30)
-    dob.Doble().insertar_inicio(40)
-    dob.Doble().insertar_inicio(50)
-    dob.Doble().imprimir()
-    # 5 INSERCIONES AL FINAL
-    """dob.Doble().insertar_final(100)
-    dob.Doble().insertar_final(200)
-    dob.Doble().insertar_final(300)
-    dob.Doble().insertar_final(400)
-    dob.Doble().insertar_final(500)"""
-   # GRAFICAR
-    #dob.Doble().graficar()
-    # 5 INSERCIONES EN POSICIONES
-    # GRAFICAR
-    #dob.Doble().graficar()
-    # OBTENER VALOR
-    # ELIMINAR POR MEDIO DEL INDICE
-    # GRAFICAR
-    #dob.Doble().graficar()
+    cp = clasePrincipal()
+    cp.metodoUnico()
